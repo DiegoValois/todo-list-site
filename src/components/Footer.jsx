8689b5410c964cd58792978764438241fdb9c1dc
mvci <valois.marcos@gmail.com>
2022-12-22 21:16:49 -0500

    feat: detect user connected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer (){
   return (
-    <div class="footer">
+    <div className="footer">
         <p href="https://github.com/DiegoValois"
         style={{fontSize:"14px", cursor: "pointer", margin: "0px"}}>Diego Valois</p>
     </div>
--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -8,14 +8,14 @@ export default function Guide() {
   return (
     <>
     <Header />
-     <div class="guide-container">
+     <div className="guide-container">
       <h1>Guide</h1>
-      <div class="guide-text">
+      <div className="guide-text">
         <p>The first thing you need to do is: register on the site, or log in to your account. 
             After that, you are free to use the to-do list.</p>
       </div>
      </div>
-     <div class="guide-steps">
+     <div className="guide-steps">
         <h1><FontAwesomeIcon icon={faArrowDown} /> Click To Start</h1>
         <h1><FontAwesomeIcon icon={faArrowDown} /> Register / Log in</h1>
         <h1><FontAwesomeIcon icon={faArrowDown} /> Click In Join</h1>
